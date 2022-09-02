fb107e58894fd4d5dba382e9c9495f7e584e9e9e
ArkadiNazarian <nazarianarkadi@gmail.com>
2022-09-02 15:28:58 +0430

    contact with me is updated

--- src/Contact/view.tsx
+++ src/Contact/view.tsx
@@ -1,62 +1,37 @@
-import { MdPersonPin } from "react-icons/md";
+import { BiSend } from "react-icons/bi";
 import { RiPhoneLine } from "react-icons/ri";
 import { SiGmail } from "react-icons/si";
 import { IProps } from "./model";
 
 export const View = (props: IProps) => {
     return (
-        <div className="tw-pt-20">
-            <div className="tw-text-center ">
+        <div className="tw-pt-20 tw-mb-8">
+            <div className="tw-text-center">
                 <p className="tw-text-4xl tw-text-primary">Contact Me</p>
                 <p className="tw-text-xl tw-text-secondary">Get in touch</p>
             </div>
-            <div className="tw-grid tw-grid-cols-3">
-                <div className="tw-flex tw-items-center tw-col-start-1">
-                    <span className="tw-text-purple tw-text-3xl tw-mr-3">
-                        <MdPersonPin />
-                    </span>
-                    <p className="tw-text-2xl tw-text-primary tw-mr-5">Arkadi</p>
+            <form ref={props.form} onSubmit={props.action_submit}>
+                <div>
+                    <input
+                        placeholder="Email"
+                        className="tw-mb-8 tw-pl-2 tw-text-xl tw-placeholder-background tw-bg-secondary tw-border-2 border-slate-300 tw-w-1/4 tw-rounded-md tw-h-10"
+                        name="email"
+                        type="email"
+                        value={props.form_data.email}
+                        onChange={props.handleChange}
+                    />
                 </div>
-                <div className="tw-flex tw-items-center tw-col-start-2">
-                    <span className="tw-text-purple tw-text-3xl tw-mr-3">
-                        <RiPhoneLine />
-                    </span>
-                    <p className="tw-text-2xl tw-text-primary tw-mr-5">+989366057725</p>
+                <div>
+                    <textarea
+                        placeholder="Message"
+                        className="tw-mb-8 tw-text-2xl tw-placeholder-background tw-pt-2 tw-pl-1 tw-bg-secondary tw-border-2 border-slate-300 tw-w-1/3 tw-rounded-md tw-h-32"
+                        name="message"
+                        value={props.form_data.message}
+                        onChange={props.handleChange}
+                    />
                 </div>
-
-                <div className="tw-flex tw-items-center tw-col-start-3">
-                    <span className="tw-text-purple tw-text-3xl tw-mr-3">
-                        <SiGmail />
-                    </span>
-                    <p className="tw-text-2xl tw-text-primary ">nazarianarkadi@gmail.com</p>
-                </div>
-            </div>
-            <div>
-                <form ref={props.form} onSubmit={props.action_submit}>
-                    <div>
-                        <input
-                            name="message"
-                            type="text"
-                            value={props.form_data.message}
-                            onChange={props.handleChange}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            name="email"
-                            type="text"
-                            value={props.form_data.email}
-                            onChange={props.handleChange}
-                        />
-                    </div>
-                    <div>
-                        {
-                            props.form_errors.message
-                        }
-                    </div>
-                    <button type="submit">Submit</button>
-                </form>
-            </div>
-        </div >
+                <button type="submit" className=" tw-rounded-md tw-h-16 tw-w-32 tw-text-xl tw-flex tw-justify-center tw-items-center tw-bg-purple tw-text-primary">Submit <BiSend className="tw-pl-2 tw-text-2xl" /></button>
+            </form>
+        </div>
     )
 }
